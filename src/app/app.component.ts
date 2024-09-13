02a8566849d4d188cff6335ea0cc380bc300ce06
George Essam <georgeessam00@gmail.com>
2024-09-14 02:08:06 +0300

    add main section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavComponent],
+  imports: [RouterOutlet,NavComponent,MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
