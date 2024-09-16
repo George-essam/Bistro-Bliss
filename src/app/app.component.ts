25186fc64dd014464866f605fc03fb042ae67253
George Essam <georgeessam00@gmail.com>
2024-09-16 16:15:42 +0300

    adding new section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { MainComponent } from './main/main.component';
+import { FirstsectionComponent } from './firstsection/firstsection.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavComponent,MainComponent],
+  imports: [RouterOutlet,NavComponent,MainComponent,FirstsectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/firstsection/firstsection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firstsection',
+  standalone: true,
+  imports: [],
+  templateUrl: './firstsection.component.html',
+  styleUrl: './firstsection.component.css'
+})
+export class FirstsectionComponent {
+
+}
