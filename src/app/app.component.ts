049e7ae6e6bcb7452a957cef30220a4aadb105fb
George Essam <georgeessam00@gmail.com>
2024-09-18 09:46:45 +0300

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { MainComponent } from './main/main.component';
 import { FirstsectionComponent } from './firstsection/firstsection.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavComponent,MainComponent,FirstsectionComponent],
+  imports: [RouterOutlet,NavComponent,MainComponent,FirstsectionComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
